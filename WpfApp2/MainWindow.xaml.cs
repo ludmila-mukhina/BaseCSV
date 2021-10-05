b6c441d0dde8b9730b81d1a32867aa4cfdb41a78
МухинаЛВ@ngknn.local <@ngknn.local>
2021-10-05 10:17:31 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string path = "Base.csv";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnWrite_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter(path)) 
+            {
+                sw.Write(TBName.Text+";");
+                sw.Write(DPDr.SelectedDate.ToString() + ";");
+                if (RBMen.IsChecked==true)
+                {
+                    sw.Write("Мужской" + ";");
+                }
+                if (RBWomen.IsChecked==true)
+                {
+                    sw.Write("Женский" + ";");
+                }
+
+            }
+        }
+    }
+}
