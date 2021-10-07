5150a5fa3c73b17e1e4ab2ab7c065588d4c9d7f9
lyudmila.archackowa <lyudmila.archackowa@yandex.ru>
2021-10-07 14:59:26 +0300

    Добавлен просмотр записей и полоса прокрутки

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -22,14 +22,21 @@ namespace WpfApp2
     public partial class MainWindow : Window
     {
         string path = "Base.csv";  // путь у файлу
+        List<People> ListPeople = new List<People>();
         public MainWindow()
         {
             InitializeComponent();
+            if (File.Exists(path))
+            {
+                BtnShow.IsEnabled = true;
+            }
         }
 
         private void BtnWrite_Click(object sender, RoutedEventArgs e)
         {
-            using (StreamWriter sw = new StreamWriter(path)) // запись в scv-файл
+            Im.Visibility = Visibility.Visible;
+            SPShow.Visibility = Visibility.Collapsed;
+            using (StreamWriter sw = new StreamWriter(path, true)) // запись в scv-файл
             {
                 sw.Write(TBName.Text+";");    // запись в csv-файл из текстового поля
                 sw.Write(DPDr.SelectedDate.ToString() + ";");   // запись в файл даты, выбранной в календаре
@@ -42,7 +49,83 @@ namespace WpfApp2
                 {
                     sw.Write("Женский" + ";");
                 }
+                string character = "";
+                if (CBСheerful.IsChecked==true)
+                {
+                    character += CBСheerful.Content+" ";
+                }
+                if (CBCharismatic.IsChecked==true)
+                {
+                    character += CBCharismatic.Content + " ";
+                }
+                if (CBStubborn.IsChecked==true)
+                {
+                    character += character + CBStubborn.Content + " ";
+                }
+                character = character.Substring(0, character.Length - 1);
+                sw.Write(character + ";");
+                ListBoxItem lb = (ListBoxItem)LBSp.SelectedItem;
+                sw.Write(lb.Content+"\n");
+                MessageBox.Show("Данные записаны");
+                BtnShow.IsEnabled = true;
+                TBName.Text = "";
+            }
+        }
+        int i;
+        private void BtnShow_Click(object sender, RoutedEventArgs e)
+        {
+            Im.Visibility = Visibility.Collapsed;
+            SPShow.Visibility = Visibility.Visible;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while(sr.EndOfStream!=true)
+                {
+                    string[] Sarr = sr.ReadLine().Split(';');
+                    ListPeople.Add(new People() 
+                    { Name = Sarr[0], Dr = Sarr[1], Gender = Sarr[2], Character = Sarr[3], Sp = Sarr[4] });
 
+                }
+            }
+            i = 0;
+            TBShowName.Text = ListPeople[0].Name;
+            TBShowDR.Text = ListPeople[0].Dr;
+            TBShowGender.Text = ListPeople[0].Gender;
+            TBShowCharacter.Text = ListPeople[0].Character;
+            TBShowSp.Text = ListPeople[0].Sp;
+
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)  // следующая запись
+        {
+            try
+            {
+                i++;
+                TBShowName.Text = ListPeople[i].Name;
+                TBShowDR.Text = ListPeople[i].Dr;
+                TBShowGender.Text = ListPeople[i].Gender;
+                TBShowCharacter.Text = ListPeople[i].Character;
+                TBShowSp.Text = ListPeople[i].Sp;
+            }
+            catch
+            {
+                MessageBox.Show("Это была последняя запись");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                i = ListPeople.Count-1;
+                TBShowName.Text = ListPeople[i].Name;
+                TBShowDR.Text = ListPeople[i].Dr;
+                TBShowGender.Text = ListPeople[i].Gender;
+                TBShowCharacter.Text = ListPeople[i].Character;
+                TBShowSp.Text = ListPeople[i].Sp;
+            }
+            catch
+            {
+                MessageBox.Show("Это была последняя запись");
             }
         }
     }
---
+++ WpfApp2/People.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class People
+    {
+        public string Name { get; set; }
+        public string Dr { get; set; }
+        public string Gender { get; set; }
+        public string Character { get; set; }
+        public string Sp { get; set; }
+    }
+}
